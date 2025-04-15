cee6545650510c24612d2d946859792496caadd7
김주원 <hmmthinking@naver.com>
2025-04-15 06:11:38 +0000

    feat: pregentation

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,3 +1,22 @@
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
+import Fade from 'embla-carousel-fade'
+import { useSectionsStore } from "./stores/sections"
+import { usePageSettingStore } from "./stores/pageSetting"
+import Viewer from "./Viewer"
+
 export default function Presentation() {
-  return <p>dpd</p>
+  const { sectionsList } = usePageSettingStore()
+  const { sections } = useSectionsStore()
+
+  return (
+    <Carousel plugins={[ Fade() ]}>
+      <CarouselContent>
+        {sectionsList.map((id, idx) => (
+          <CarouselItem className="border" key={idx}>
+            <Viewer md={sections[id].content} assets={sections[id].assets} width={window.innerWidth} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  )
 }

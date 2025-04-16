dde629649007a19dc65d9b4783874b2f8020cbd0
김주원 <hmmthinking@naver.com>
2025-04-16 00:07:43 +0000

    feat: add keybind in presentation

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,13 +1,26 @@
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
+import { Carousel, CarouselApi, CarouselContent, CarouselItem } from "@/components/ui/carousel"
 import Fade from 'embla-carousel-fade'
 import { usePageSettingStore } from "./stores/pageSetting"
 import Viewer from "./Viewer"
+import { useEffect, useState } from "react"
 
 export default function Presentation() {
   const { sectionsList } = usePageSettingStore()
+  
+  const [api, setApi] = useState<CarouselApi>()
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') api?.scrollPrev()
+      if (e.key === 'ArrowRight') api?.scrollNext()
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [api])
 
   return (
-    <Carousel plugins={[ Fade() ]}>
+    <Carousel plugins={[ Fade() ]} setApi={setApi}>
       <CarouselContent>
         {sectionsList.map((id, idx) => (
           <CarouselItem className="border" key={idx}>
